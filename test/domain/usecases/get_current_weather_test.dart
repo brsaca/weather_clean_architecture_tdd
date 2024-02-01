ec9131eb79e68705ae1621fb343f33ba6c40326f
brsaca <brsaca@gmail.com>
2024-02-01 12:10:09 -0600

    test: user case add helper to mock repository

---
+++ test/domain/usecases/get_current_weather_test.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:weather_clean_architecture_tdd/domain/entities/weather.dart';
+import 'package:weather_clean_architecture_tdd/domain/usecases/get_current_weather.dart';
+import '../../helpers/test_helpers.mocks.dart';
+
+void main() {
+  MockWeatherRepository mockWeatherRepository = MockWeatherRepository();
+  GetCurrentWeatherUseCase getCurrentWeatherUseCase =
+      GetCurrentWeatherUseCase(mockWeatherRepository);
+
+  const testCityName = 'New York';
+
+  const testWeatherDetail = WeatherEntity(
+    cityName: 'New York',
+    main: 'Clouds',
+    description: 'few clouds',
+    iconCode: '02d',
+    temperature: 302.28,
+    pressure: 1009,
+    humidity: 70,
+  );
+
+  test('should get current weather detail from the repository', () async {
+    // arrange
+    when(mockWeatherRepository.getCurrentWeather(testCityName))
+        .thenAnswer((_) async => const Right(testWeatherDetail));
+
+    // act
+    final result = await getCurrentWeatherUseCase.execute(testCityName);
+
+    // assert
+    expect(result, const Right(testWeatherDetail));
+  });
+}
